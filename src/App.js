aa4bc7b5859c9819f0e59344afca6bb62e303b3c
Zakaria Nur <s362082@oslomet.no>
2023-01-09 01:25:29 +0100

    v1.3

--- src/App.js
+++ src/App.js
@@ -26,14 +26,12 @@ const firestore = firebase.firestore();
 
 function App() {
   const [user] = useAuthState(auth);
-
   return (
     <div className="App">
       <header>
         <svg id="logo"className='logo' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" color='#111827'>
           <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
         </svg>
-
         {user ? <SignOut /> : <SignIn />}
       </header>
       <div className='chatbox'>
@@ -47,12 +45,11 @@ function App() {
 }
 
 function SignIn() {
-
-
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
+ 
 
   return (
     <button onClick={signInWithGoogle}>Sign in with Google</button>
@@ -67,20 +64,21 @@ function SignOut() {
 }
 
 function ChatRoom() {
-
-  const refprop = useRef();
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(100);
+  const query = messagesRef.orderBy('createdAt').limit(1000);
 
+  const messagesEndRef = useRef(null);
 
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
-  if ([messages] == 10) {
-    messagesRef.endAt(10)
-  }
 
   const [formValue, setFormValue] = useState('');
+
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -94,22 +92,23 @@ function ChatRoom() {
     })
     setFormValue('');
 
-    refprop.current.scrollIntoView({ behavior: 'smooth' });
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+    
   }
 
   return (
     <>
       <main>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-        <div ref={refprop} ></div>
+        <span ref={dummy} ></span>
+        <div ref={messagesEndRef} />
       </main>
 
       <div></div>
 
       <form onSubmit={sendMessage}>
-
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+      <button onClick={scrollToBottom}>Scroll to last message</button>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Type here..."/>
         <button id='send1' type='submit'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" color='white' stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>
